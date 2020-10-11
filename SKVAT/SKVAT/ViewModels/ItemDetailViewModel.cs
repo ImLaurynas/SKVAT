9e6e0e203e3a60bd4ba7d83ac18f82c7aa2e6587
ImLaurynas <lauris1013@gmail.com>
2020-10-11 21:30:29 +0300

    first commit

---
+++ SKVAT/SKVAT/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SKVAT.Models;
+
+namespace SKVAT.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
